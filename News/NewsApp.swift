a478c0ddeb5a183e7bb17e7053e2a5be75cd274d
Michael Woo <michaelpwoo@gmail.com>
2023-10-10 17:15:53 -0700

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Michael Woo on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
